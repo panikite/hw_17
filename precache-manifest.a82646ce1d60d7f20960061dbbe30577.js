81d8ed3f289166ae382fe2c942be0cf9fc817f0a
panikite <ne.isnuje@gmail.com>
2020-09-27 17:54:56 +0300

    Updates

---
+++ precache-manifest.a82646ce1d60d7f20960061dbbe30577.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c55413df2f34a2505918f830c2f972a0",
+    "url": "/hw_17/index.html"
+  },
+  {
+    "revision": "7a48e1e330ec18aa88a0",
+    "url": "/hw_17/static/css/main.50ca22ce.chunk.css"
+  },
+  {
+    "revision": "964598070804ccbf3b59",
+    "url": "/hw_17/static/js/2.c17b38fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw_17/static/js/2.c17b38fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a48e1e330ec18aa88a0",
+    "url": "/hw_17/static/js/main.5985da9b.chunk.js"
+  },
+  {
+    "revision": "0eed67190143dfc42fb1",
+    "url": "/hw_17/static/js/runtime-main.985944b7.js"
+  }
+]);
